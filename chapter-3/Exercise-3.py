67a345c6a1a38a664de9d1e9fa1dabecfda0cad5
Ryland Rubens <39800803+ryrubes@users.noreply.github.com>
2022-04-18 13:19:47 -0700

    Update Exercise-3.py

--- chapter-3/Exercise-3.py
+++ chapter-3/Exercise-3.py
@@ -1,11 +1,11 @@
-# calculate which squaare you'll need to place a certain number of rice grains
+# calculate which square you'll need to place a certain number of rice grains
 # imagine you have a chessboard and put a single grain of rice on one square
 
 # Exercise 3:
 def chessboardSpace(numberOfGrains):
     chessboardSpaces = 1
     placedGrains = 1
-# while placed grains are less that total number of grains, multiply by 2
+# while placed grains are less than total number of grains, multiply by 2
 # and move grain up one square
     while placedGrains < numberOfGrains:
         placedGrains *= 2
